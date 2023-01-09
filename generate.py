d6f0d23eb68a3f3ba192d32919360a2c500b17be
Vishal <shrivastava_vishal@outlook.com>
2023-01-09 02:18:34 -0600

    many links - final

--- generate.py
+++ generate.py
@@ -7,16 +7,31 @@ height = 1
 x1 = 0.5
 y1= 0.5
 z1 = 0.5
-x2 = 0.5
-y2 = 1.5
+
+x2 = 1
+y2 = y1
 z2 = 1.5
-#pyrosim.Send_Cube(name="Box2", pos=[x2, y2, z2] , size=[length,width,height])
 #pyrosim.Send_Cube(name="Box", pos=[x1, y1, z1] , size=[length,width,height])
-for i in range(10):
-    pyrosim.Send_Cube(name="Box", pos=[x1, y1, z1], size=[length, width, height])
-    z1 += 1.5
-    length *= 0.9
-    width *= 0.9
-    height *= 0.9
-    
+#pyrosim.Send_Cube(name="Box2", pos=[x2, y2, z2] , size=[length,width,height])
+
+for k in range(5):
+    li = length
+    wi = width
+    hi = height
+    for j in range(5):
+        li = length
+        wi = width
+        hi = height
+        for i in range(10):
+            pyrosim.Send_Cube(name="Box", pos=[x1, y1, z1], size=[li, wi, hi])
+            z1 += hi
+            li *= 0.9
+            wi *= 0.9
+            hi *= 0.9
+        x1 += 1
+        z1 = 0.5
+    y1 += 1
+    x1 = 0.5
+    z1 = 0.5
+
 pyrosim.End()
